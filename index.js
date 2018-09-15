06d9220b7877cd83b75b39775736bd7e5c4dfd7f
SphinxNumberNine <ashwathkrishnan@yahoo.com>
2018-09-15 12:26:43 -0400

    Created routes directory and refactored index to move the route over

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send({ message: "This is still in development" });
-});
+require("./routes/testRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
