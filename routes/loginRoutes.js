9ba0c0b78157244451c49f1bc5220ba3b08b84cb
Ashwath Krishnan <ashwath@krishnan.io>
2018-10-15 11:47:03 -0400

    Registration route works with password hashing

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const User = mongoose.model("users");
 
 module.exports = app => {
-  app.post("/api/register-student", async (req, res) => { 
+  app.post("/api/register-student", async (req, res, next) => {
     if (
       req.body.email &&
       req.body.firstName &&
@@ -10,24 +10,26 @@ module.exports = app => {
       req.body.password &&
       req.body.passwordConf
     ) {
-        var userData = {
-            email: req.body.email,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            password: req.body.password,
-            passwordConf: req.body.passwordConf
-        }
+      if (req.body.password != req.body.passwordConf) {
+        var err = new Error("Passwords do not match.");
+        return next(err);
+      }
 
-        console.log(userData);
+      var userData = {
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password,
+      };
 
-        User.create(userData, async (err, user) => {
-            if(err) {
-                console.log(err);
-                return res.status(500).json({message: "Internal server error"})
-            } else {
-                return res.status(200).json({ user })
-            }
-        })
+      const user = await new User(userData).save();
+
+      res.status(200).json({ user });
+
+      next();
+    } else {
+        var err = new Error("Some required fields are empty.");
+        return next(err);
     }
   });
 };
